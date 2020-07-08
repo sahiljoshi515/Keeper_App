db88915d3ba5611b14b9b910dde6a59af3093bd5
Sahil Joshi <52909523+sahiljoshi515@users.noreply.github.com>
2020-07-08 14:19:14 +0530

    add message

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(db || process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
     //Set static folder
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 
 }
