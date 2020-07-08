81a4925f75c96aa1a34879616708fa52dba6c57d
Sahil Joshi <52909523+sahiljoshi515@users.noreply.github.com>
2020-07-08 13:00:12 +0530

    Fix errors

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const notes = require('./routes/api/notes');
 
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 //BodyParser Middleware
 app.use(bodyParser.json());
@@ -26,10 +27,10 @@ if (process.env.NODE_ENV === 'production') {
     //Set static folder
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 
 }
 
-const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
